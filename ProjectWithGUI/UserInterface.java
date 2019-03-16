c12cd82249e5ec206416d17b005c65e2fcbfcf61
Hongjian Gu <hongjian.gu@mail.mcgill.ca>
2019-03-15 21:56:45 -0400

    Add files via upload

--- ProjectWithGUI/UserInterface.java
+++ ProjectWithGUI/UserInterface.java
@@ -1,25 +1,27 @@
-package ProjectWithGUI;
-
-import java.awt.Component;
+import java.awt.Color;
 import java.awt.Font;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
 public class UserInterface extends JFrame{
 
 	private JPanel pvisitor;
 	private JPanel pstaff;
-	private static Map <String, JPanel> panelMap=new HashMap();
-	
-	static final int TABSIZE=20;  
-	static final int BWIDTH = 250;
-	static final int BLENGTH = 40;
+	static final int TABSIZE=20;
+	private static final String PWD = "COMP421G66";
+    private JPasswordField password;
+    private String typedPassword;
 	
 	UserInterface(){
 		
@@ -39,36 +41,75 @@ public class UserInterface extends JFrame{
 	    setVisible(true);  
 	    
 	    tp.setFont(new Font("TimesRoman", Font.BOLD, TABSIZE));
-	    
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
 
 	
 	void initVisitor(){
-		pvisitor.setLayout(null);
 		
-		JButton b1=new JButton("Search Activities");
-		//b1.addActionListener(l);
-		
-		
-		b1.setBounds(250, 150 , 300, 50);
-		b1.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		
-		JButton b2=new JButton("Make a reservation");
-		b2.setBounds(250, 250 , 300, 50);
-		b2.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		
-		pvisitor.add(b1);
-		pvisitor.add(b2);
-
+		pvisitor.add(new myButton("Search activities"));
 		
 	}
 	
 	void initStaff(){
+		// Login button
+		JButton login = new JButton("Login");
+		login.setFont(new Font("TimesRoman", Font.BOLD, 24));
+		login.setBounds(300,500,300,40);
+		// Text label
+		JLabel txt = new JLabel();
+		txt.setText("Please Enter Password :");
+		txt.setFont(new Font("TimesRoman", Font.BOLD, 48));
+		txt.setBounds(100, 200, 600, 70);
+		// Empty label to display password feedback
+		JLabel feedback = new JLabel();
+		feedback.setBounds(400, 650, 200, 100);
+		// Initialize password field
+		ActionListener pwdListener = new ActionListener() {
+			
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	
+	            password = (JPasswordField) e.getSource();
+	            char [] tempPass = password.getPassword();
+	            typedPassword = new String(tempPass);
+	
+	            if (!typedPassword.equals(PWD)){
+	
+	                JOptionPane.showMessageDialog(null,
+	            "Your password is not correct",
+	            "WRONG PASSWORD!",
+	            JOptionPane.ERROR_MESSAGE);
+	            }
+	        }
+	    };
+	    
+	    createPasswordField(pwdListener);
 		
+		// Add to frame
+		pstaff.add(feedback);
+		pstaff.add(password);
+		pstaff.add(txt);
+		pstaff.add(login);    
+		pstaff.setSize(300,300);    
+		pstaff.setLayout(null);    
+		pstaff.setVisible(true);
+		// Action listener
+		login.addActionListener(pwdListener);      
 	}
 	
+	// Create a password field
+    public void createPasswordField(ActionListener pwdListener){
+
+	    password = new JPasswordField(30);
+	    password.setBounds(180, 300, 400, 70);
+	    password.setEchoChar('*');
+	    password.setBackground(Color.white);
+	 	password.setFont(new Font("TimesRoman", Font.PLAIN ,36));
+	    password.addActionListener(pwdListener);
+	}
+
+
 	
 	//the windows displace alternative options 
 	//by default in visitor mode
