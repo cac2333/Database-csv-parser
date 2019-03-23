37f7dafcc0834e65fad4febe13623dc7b8b87a62
cac2333 <xiaohan.wang2@mail.mcgill.ca>
2019-03-23 00:05:55 -0400

    1

---
+++ src/ProjectWithGUI/Database.java
@@ -0,0 +1,455 @@
+package ProjectWithGUI;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Database {
+	
+	private Statement statement;
+	private ResultSet rset;
+	private Connection con;
+
+	private int sqlCode=0; //Variable to hold SQLCODE
+	private String sqlState="00000"; //var to hold SQLSTATE
+
+	private Database(){
+		//Unique table name. 
+		//Either the user supplies a unique identifier as a command line arg, 
+		//or the program makes one up
+
+		String tableName="";
+		Scanner sc;
+		String usernamestring="";
+		String passwordstring="";
+		
+		try {
+			sc = new Scanner(new File("data\\account.txt"));
+			sc.useDelimiter("...");
+			usernamestring=sc.nextLine();
+			passwordstring=sc.nextLine();
+			sc.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("File Not Found");
+		}
+
+		try {
+			DriverManager.registerDriver ( new org.postgresql.Driver() ) ;
+		} catch (Exception cnfe){
+			System.out.println("Class not found");
+		}
+
+		// This is the url you must use for Postgresql.
+		//Note: This url may not valid now !
+		
+		String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+		
+		try {
+			con = DriverManager.getConnection (url,usernamestring, passwordstring);
+			statement = con.createStatement ( ) ;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	public void quit(){
+		
+		try {
+			statement.close();
+			rset.close();
+			con.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public static Database getInstance(){
+		return new Database();
+	}
+	
+	
+	private void insert(Statement statement, String tableName, String val)throws SQLException{
+		
+		String insertSQL;
+		try{
+
+				insertSQL = "INSERT INTO "+tableName+" VALUES("+val+");";
+				System.out.println(insertSQL);
+				statement.executeUpdate(insertSQL);
+				System.out.println("DONE");
+				
+		}catch(SQLException e){
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+		}
+	}
+	
+	
+	//Q2 part 2 (modification) insert a new activity
+		public String insertActivity(String activityName, String activityTime, String inputPrice, String areaName) { 
+
+			
+			//parse price
+			double newPrice=0.0;
+			try {
+				newPrice = Double.parseDouble(inputPrice);
+			}catch(InputMismatchException e) {
+				return "Please type in a valid price";
+			}
+			
+			//append default hh:mm:ss to the time
+			activityTime+=" 09:00:01";
+			
+			//insert an activity
+			try {
+				insert(statement, "Activity", "'"+activityName+"','"+activityTime+"',"+newPrice+"");
+				
+			}catch(SQLException e) {
+				//stop inserting if an error occurs
+				
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				
+				if(sqlCode==22007) {
+					return "Please type in a valid date";
+				}
+				
+				return ("Code: " + sqlCode + "  sqlState: " + sqlState);
+			}
+			
+
+			//insert into takePlacesIn as well
+			try {
+				insert(statement, "takePlacesIn", "'"+activityName+"','"+activityTime+"','"+areaName+"'");
+				
+			}catch(SQLException e) {
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				return ("Code: " + sqlCode + "  sqlState: " + sqlState);
+			}
+			
+			
+			
+			
+			return "Successfully inserted your activity!";
+		}
+		
+		
+		//Q2 part 5 (modification) modify a price
+		public String modifyPrice(String activityName, String activityTime, String inputPrice) {
+			//parse price
+			double newPrice=0.0;
+			try {
+				newPrice = Double.parseDouble(inputPrice);
+			}catch(InputMismatchException e) {
+				return "Please type in a valid new price";
+			}
+			
+			String tempQuery = "UPDATE activity SET price ="+newPrice
+					+" WHERE activityName ='"+activityName+"' AND activityTime='"
+					+activityTime+"'";
+			try {
+				int	result = statement.executeUpdate(tempQuery);
+				//System.out.println(result);
+				if(result==0) {
+					return "Modification failed. No such an activity";
+				}
+			}catch(SQLException e) {
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				//return "Successfully modified the price of the activity you've specified";//hard code
+				System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+				return "Modification failed. No such an activity";
+			}		
+			System.out.println("Successfully modified the price of the activity you've specified");
+			return "Successfully modified the price of the activity you've specified";
+		}		
+		
+		
+	//Q2 PART 1 (a) (query) select activities according to user’s search 
+	public String[][] selectActivities(String inputDate) {
+		ArrayList<String[]> rowList=new ArrayList<String[]>();
+		
+		//append hh:mm:ss
+		String start = inputDate+" 00:00:00";
+		String end   = inputDate+" 23:59:59";
+		
+		String tempQuery = "SELECT * FROM activity WHERE activityTime BETWEEN '"+start+"' AND '"+end+"'";
+		
+		try {
+			rset = statement.executeQuery(tempQuery);
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+		}
+		
+		try {
+			if (!rset.next()) {                            //if rs.next() returns false
+	            //then there are no rows.
+				String[][] immediateResult = null;
+				return immediateResult;
+			}
+			else {
+				
+					do {
+						String actName = rset.getString("activityName");
+						String actTime = rset.getString("activityTime");
+						double actPrice = rset.getDouble("price");
+						String[] oneRow = new String[3];
+						oneRow[0]=actName;
+						oneRow[1]=actTime;
+						oneRow[2]=""+actPrice;
+						rowList.add(oneRow);
+					} while (rset.next());
+			}			
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+		}
+		
+		String[][] result = rowList.toArray(new String[0][]);
+		
+		return result;
+	}
+	
+		
+		
+		
+		
+		
+		//Q2 PART 1 (b) (modification) insert a new reservation () 
+		public String insertReservation(String currentTime, String phone, String inputNOP,String actPrice, String actName,String actTime) {
+			int nofppl=0;
+			try {
+				nofppl = Integer.parseInt(inputNOP);
+			}catch(InputMismatchException e) {
+				return "Please type in a valid new no. of people";
+			}
+			
+			//parse price
+			double newPrice=0.0;
+			try {
+				newPrice = Double.parseDouble(actPrice);
+			}catch(InputMismatchException e) {
+				return "Please type in a valid price";
+			}
+			double totalPrice = newPrice*nofppl;
+			
+			
+			String tempQuery = "SELECT MAX(booknumber) as a FROM book";
+			int newBookNumber = 100214;
+			try {
+				rset = statement.executeQuery(tempQuery);
+				if(!rset.next()) {
+					return "Error occurred";
+				}else {
+					newBookNumber = rset.getInt("a")+1;
+					insert(statement,"reservation",""+newBookNumber+","+nofppl+","+totalPrice+",'"+actName+"','"+actTime+"'");
+					
+				}	
+			}catch(SQLException e) {
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+			}
+			
+			//insert into book as well!
+			insertBook(phone,currentTime,newBookNumber);
+			
+			
+			return "Successfully reserved!";
+		}
+		
+		public String insertBook(String phone, String bookTime, int newBookNumber) {
+			String tempQuery="";
+			try {
+				tempQuery =""+newBookNumber+",'"+phone+"','"+bookTime+"'";
+				insert(statement, "book",tempQuery);
+			}catch(SQLException e) {
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+			}
+			return "";
+		}
+		
+		//stored procedure calling
+		public String storedProcedure(String inputDate) {
+			String query = "SELECT assistanceList('"+inputDate+"')";
+			String result = "";
+			try {
+				rset = statement.executeQuery(query);
+				if(!rset.next()) {
+					return "";
+				}else {
+					result = rset.getString(1);
+				}
+			}catch(SQLException e) {
+				sqlCode = e.getErrorCode(); // Get SQLCODE
+				sqlState = e.getSQLState(); // Get SQLSTATE
+				System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+			}
+			
+			return result;
+		}
+	
+	
+	// Q2.4 (query) select overall income over a specific time period
+	public String selectIncome(String startDate, String endDate) {
+		
+		String selectSQL;
+		String result = null;
+		
+		if (startDate.compareTo(endDate) == 1) {
+			return "Please enter a valid date.";
+		}
+		
+		try {
+			// SELECT totalPrice FROM Reservation
+			// WHERE activityTime >= startDate AND activityTime <= endDate
+			selectSQL = "SELECT SUM(totalPrice) FROM Reservation WHERE activityTime >= '" + startDate +
+					"' AND activityTime <= '" + endDate + "'";
+			System.out.println(selectSQL);
+			rset = statement.executeQuery(selectSQL);
+			
+			if(!rset.next()){
+				return null;
+			}
+			
+			result = rset.getString(1);
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+			if (sqlCode == 39004) {
+				return "null value not allowed";
+			}
+			if (sqlCode == 42804) {
+				return "Data type mismatch";
+			}
+			if (sqlCode == 42804) {
+				return "Data type mismatch";
+			}
+			
+		}
+		
+		return result;
+	}
+	
+	
+	public String addStaff(String staffname) {
+		String tempQuery = "SELECT MAX(staffid) FROM STAFF";
+		int newStaffID=0;
+		try {
+			rset = statement.executeQuery(tempQuery);
+			if(!rset.next()) {
+				return "";
+			}else {
+				newStaffID = rset.getInt(1)+1;
+				insert(statement,"staff",""+newStaffID+",'"+staffname+"'");
+			}
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+		}
+		return "Successful! Welcome to our family!";
+	}
+	
+	
+	public String deleteStaff(String staffID, String staffname, String replaceID) {
+		//sanity check
+		String tempQuery ="";
+		try {
+			tempQuery="SELECT staffid,staffname FROM staff WHERE staffid="+staffID
+					+" AND staffname='"+staffname+"'";
+			rset = statement.executeQuery(tempQuery);
+			if(!rset.next()) {
+				return "Failed. NON-EXISTENT";
+			}else {
+				//make sure this is the right staff to fire
+				if(staffID.equals(""+rset.getInt(1)) && rset.getString(2).equals(staffname)) {
+					//update manage
+					statement.executeUpdate("UPDATE MANAGE " + 
+							"SET STAFFID=" + replaceID
+							+" WHERE STAFFID="+staffID);
+					
+					//update tour guide
+					rset = statement.executeQuery("SELECT * FROM tourguide WHERE staffid="+staffID);
+					if(rset.next()) {
+						rset =statement.executeQuery("SELECT * FROM tourguide WHERE staffid="+replaceID);
+						if(!rset.next()) {
+							//insert the replace id into tour guide
+							insert(statement,"tourguide",replaceID);
+						}
+							
+						//update guidedTour
+						statement.executeUpdate("UPDATE guidedTour " + 
+								"SET tourguideID=" + replaceID
+								+" WHERE tourguideID="+staffID);
+						//update tourGuide
+						rset =statement.executeQuery("SELECT * FROM tourguide WHERE staffid="+replaceID);
+						if(!rset.next()) {
+							statement.executeUpdate("UPDATE tourGuide " + 
+									"SET STAFFID=" + replaceID
+									+" WHERE STAFFID="+staffID);
+						}else {
+							//delete from tourguide
+							statement.executeUpdate("DELETE FROM tourguide WHERE staffid="+staffID);
+						}
+
+					}
+					
+					//delete the staff
+					statement.executeUpdate("DELETE FROM STAFF WHERE staffid="+staffID);
+					
+				}else {
+					return "Failed. NON-EXISTENT";
+				}
+			}
+			
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+			return "Failed. NON-EXISTENT";
+		}
+		
+		return "Successful";
+	}
+	
+	public void insertVisitor(String phone, String name, String notes) {
+		
+		try {
+			insert(statement,"visitor","'"+phone+"','"+name+"','"+notes+"'");
+			
+		}catch(SQLException e) {
+			sqlCode = e.getErrorCode(); // Get SQLCODE
+			sqlState = e.getSQLState(); // Get SQLSTATE
+			System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/ProjectWithGUI/UserInterface.java
+++ src/ProjectWithGUI/UserInterface.java
@@ -1,10 +1,13 @@
-import java.awt.Color;
+package ProjectWithGUI;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Calendar;
 
 import javax.swing.JButton;
@@ -17,8 +20,13 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -103,44 +111,11 @@ public class UserInterface extends JFrame implements Runnable{
 		
 		pvisitor.add(b1);
 		
-	}
-	
-	private JLabel fieldName(String name, int x, int y){
-		JLabel label=new JLabel(name);
-		label.setFont(new Font("TimesRoman", Font.BOLD, 16));
-		label.setBounds(x,y,100,40);
-		return label;
-	}
-	
-	
-	void displayActivities(String date){
-		
-		pvisitor.removeAll();
-		
-			
-		String[][] activityList=db.selectActivities(date);
-			
-			
-		
+		repaint();
+		revalidate();
 		
 	}
 	
-	
-	
-	
-	void checkBoxBlock(String[] list, int x, int y){
-		
-		int side=FRAME_LENGTH-200;
-		int margin=side/list.length;
-		JPanel bg=new JPanel();
-		
-		bg.setBounds(10, 10, FRAME_WIDTH-20, FRAME_LENGTH);
-		JScrollPane scrollArea = new JScrollPane(bg);  
-	
-		
-		
-	}
-
 
 
 	@Override
@@ -950,24 +925,271 @@ public class UserInterface extends JFrame implements Runnable{
 		revalidate();
 	}
 
-	
-	//the windows displace alternative options 
-	//by default in visitor mode
-	void entry(){
-		
-	}
-	
-	
 	//void 
 	void searchActivity(){
 		
-	}
+		//clear the panel
+		pvisitor.removeAll();
+		
+		JLabel msg=new JLabel("Please select a date");
+		msg.setFont(new Font("TimesRoman", Font.BOLD, 28));
+		msg.setBounds(200,80,350,40);
+		
+		
+		// Year
+		
+		String[] year=new String[Calendar.getInstance().get(Calendar.YEAR)-2010+1];
+		for(int i=2010, k=0; i<=Calendar.getInstance().get(Calendar.YEAR); i++, k++){
+			year[k]=i+"";
+		}
+   
+		JComboBox yearBox=new JComboBox(year);
+		JLabel yearField=fieldName("Year", 200, 170);
+		yearBox.setFont(new Font(yearBox.getFont().getFontName(), Font.PLAIN, 16));
+		yearBox.setBounds(300, 170 , 200, 30);
+
+		
+		//Month 
+		
+		String[] month=new String[12];
+		for(int i=0; i<12;){
+			month[i]=++i+"";
+		}
+
+		JComboBox monthBox=new JComboBox(month);
+		JLabel monthField=fieldName("Month", 200, 240);
+		monthBox.setFont(new Font(monthBox.getFont().getFontName(), Font.PLAIN, 16));
+		monthBox.setBounds(300, 240 , 200, 30);
+		
+		
+		//Day
+		
+		String[] day=new String[31];
+		for(int i=0; i<31;){
+			day[i]=++i+"";
+		}
+
+		JComboBox dayBox=new JComboBox(day);
+		JLabel dayField=fieldName("Day", 200, 310);
+		dayBox.setFont(new Font(dayBox.getFont().getFontName(), Font.PLAIN, 16));
+		dayBox.setBounds(300, 310 , 200, 30);
+		
+		
+		//search
+		
+		JButton search=new JButton("Search");
+		search.setBounds(300, 400 , 200, 50);
+		search.setFont(new Font("TimesRoman", Font.PLAIN, 25));
+		search.addActionListener(new ActionListener (){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
 	
-	
-	void book(){
+				displayActivities(yearBox.getSelectedItem().toString()+
+						'-'+monthBox.getSelectedItem().toString()
+						+'-'+dayBox.getSelectedItem().toString());
+			}
+		});
+		
+		
+		pvisitor.add(yearBox);
+		pvisitor.add(monthBox);
+		pvisitor.add(dayBox);
+		pvisitor.add(msg);	
+		pvisitor.add(dayField);
+		pvisitor.add(monthField);
+		pvisitor.add(yearField);
+		pvisitor.add(search);
+		
+		//tells Swing this area is dirty
+		repaint();
+		//recompute the layout
+		revalidate();
 		
 	}
 	
 	
+	private JLabel fieldName(String name, int x, int y){
+		JLabel label=new JLabel(name);
+		label.setFont(new Font("TimesRoman", Font.BOLD, 16));
+		label.setBounds(x,y,100,40);
+		return label;
+	}
+	
+	
+
+	void book(String name, String time, String price){
+
+		pvisitor.removeAll();		
+		
+		JLabel msg1=new JLabel(name+" - "+time+" - \n"+"Price: "+price);
+		msg1.setFont(new Font("TimesRoman", Font.BOLD, 28));
+		msg1.setBounds(50,50,650,40);
+		
+		JLabel msg2=new JLabel("Price: "+price);
+		msg2.setFont(new Font("TimesRoman", Font.BOLD, 28));
+		msg2.setBounds(50,90,650,40);
+		
+		JLabel phoneMsg=new JLabel("Please enter your phone");
+		phoneMsg.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		phoneMsg.setBounds(50,200,250,40);
+		JTextField phone= new JTextField();
+		phone.setBounds(300, 200, 400, 40);
+		phone.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		
+		JLabel nameMsg=new JLabel("Please enter your name");
+		nameMsg.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		nameMsg.setBounds(50,250,250,40);
+		JTextField pname= new JTextField();
+		pname.setBounds(300, 250, 400, 40);
+		pname.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		
+		JLabel numMsg=new JLabel("Number of people");
+		numMsg.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		numMsg.setBounds(50,300,250,40);
+		JTextField num= new JTextField();
+		num.setBounds(300, 300, 400, 40);
+		num.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		
+		JLabel noteMsg=new JLabel("Note (Optional)");
+		noteMsg.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		noteMsg.setBounds(50,350,250,40);
+		JTextField note= new JTextField();
+		note.setBounds(300, 350, 400, 40);
+		note.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+		
+		JButton confirm=new JButton("Confirm");
+		confirm.setBounds(300, 450 , 200, 50);
+		confirm.setFont(new Font("TimesRoman", Font.PLAIN, 25));
+		confirm.addActionListener(new ActionListener (){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//get current time
+				String now=LocalTime.now().toString();
+				String date=LocalDate.now().toString();
+				String str=date+" "+now.substring(0, now.length()-4);
+				System.out.println(str);
+				
+				db.insertVisitor(phone.getText(), pname.getText(), note.getText());
+				String msg=db.insertReservation(str, phone.getText(), 
+						num.getText(), price, name, time);
+				JOptionPane.showMessageDialog(new JFrame(), msg);
+				
+			}
+		});
+		
+		JButton search=new JButton("Back");
+		search.setBounds(300, 600 , 200, 50);
+		search.setFont(new Font("TimesRoman", Font.PLAIN, 25));
+		search.addActionListener(new ActionListener (){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				initVisitor();
+			}
+		});
+		
+		
+		pvisitor.add(msg1);
+		pvisitor.add(msg2);
+		pvisitor.add(phoneMsg);
+		pvisitor.add(phone);
+		pvisitor.add(numMsg);
+		pvisitor.add(num);
+		pvisitor.add(nameMsg);
+		pvisitor.add(pname);
+		pvisitor.add(noteMsg);
+		pvisitor.add(note);
+		pvisitor.add(confirm);
+		pvisitor.add(search);
+		
+		//tells Swing this area is dirty
+		repaint();
+		//recompute the layout
+		revalidate();
+		
+	}
+	
+	
+	
+	
+	void displayActivities(String date){
+		
+		pvisitor.removeAll();
+		
+			
+		//send the query information to database
+		String[][] activityList=db.selectActivities(date);
+		
+		if(activityList==null){
+			
+			JLabel msg=new JLabel("No activity has been found");
+			msg.setFont(new Font("TimesRoman", Font.BOLD, 28));
+			msg.setBounds(200,200,400,60);
+			pvisitor.add(msg);
+			
+			
+		}else{
+			
+			listTable(activityList, 10, 10);
+			
+		}
+		
+		JButton search=new JButton("Back");
+		search.setBounds(300, 600 , 200, 50);
+		search.setFont(new Font("TimesRoman", Font.PLAIN, 25));
+		search.addActionListener(new ActionListener (){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				searchActivity();
+			}
+		});
+		
+		pvisitor.add(search);
+			
+		//tells Swing this area is dirty
+		repaint();
+		//recompute the layout
+		revalidate();
+		
+		
+	}
+	
+	
+	
+	
+	void listTable(String[][] list, int x, int y){
+		
+		//scrolll pane layout
+		
+		String[] title={"Activity", "Time", "Price"};
+		
+		JTable table=new JTable(list, title);
+		table.setRowSelectionAllowed(true);
+		table.setRowHeight(22);
+		table.setRowMargin(2);
+		table.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		table.addColumnSelectionInterval(0, list[0].length-1);
+		
+		ListSelectionModel select=table.getSelectionModel();
+		select.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		select.addListSelectionListener(new ListSelectionListener(){
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				int row=table.getSelectedRow();
+				book(table.getValueAt(row, 0).toString(), 
+						table.getValueAt(row, 1).toString(), 
+						table.getValueAt(row, 2).toString());
+			}
+			
+		});
+		
+		JScrollPane scrollArea = new JScrollPane(table);  
+		scrollArea.setBounds(x, y, FRAME_WIDTH-100, FRAME_LENGTH-250 );
+  
+		pvisitor.add(scrollArea);
+	
+		
+	}
+	
+
 	
 }
